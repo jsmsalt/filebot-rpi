1a72ed8defc1feb0404008eaeb906ee6d21ce96b
José Morales <jsmsalt@gmail.com>
2019-06-01 14:58:56 -0300

    Dynamic rename

--- filebot.sh
+++ filebot.sh
@@ -6,10 +6,10 @@ OUTPUT="/media"
 
 # Formats
 # e.g. /media/movies/Titanic (1997)/Titanic.mp4
-MOVIES_FORMAT="movieFormat=$OUTPUT/movies/{n} ({y})/{n}"
+MOVIES_FORMAT="movieFormat=$OUTPUT/movies/{any{localize[fn.match(/English/Spanish/French|German/)].name}{n}} ({y})/{any{localize[fn.match(/English/Spanish/French|German/)].name}{n}}"
 
 # e.g. /media/tvshows/Game of Thrones (2011)/Season 01/Game of Thrones s01e01.mp4
-TVSHOWS_FORMAT="seriesFormat=$OUTPUT/tvshows/{n} ({y})/Season {s.pad(2)}/{n} {s00e00}"
+TVSHOWS_FORMAT="seriesFormat=$OUTPUT/tvshows/{any{localize[fn.match(/English/Spanish/French|German/)].name}{n}} ({y})/Season {s.pad(2)}/{any{localize[fn.match(/English/Spanish/French|German/)].name}{n}} {s00e00}"
 
 # Example command. This will download all files and metadata,
 # subtitles, rename all files and move them to the specified folders.
